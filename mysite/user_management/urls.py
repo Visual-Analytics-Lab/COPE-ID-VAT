cf150558fa37a52eeeded6577f72b1f19e103797
Jacob Adams <jra457@msstate.edu>
2024-05-20 14:42:36 -0500

    Added missing / after login and logout

--- mysite/user_management/urls.py
+++ mysite/user_management/urls.py
@@ -6,8 +6,8 @@ app_name = 'user_management'  # here for namespacing of urls.
 
 urlpatterns = [
     path("register/", views.register, name="register"),
-    path("logout", views.logout_request, name="logout"),
-    path("login", views.login_request, name="login"),
+    path("logout/", views.logout_request, name="logout"),
+    path("login/", views.login_request, name="login"),
     path("manage_users/", views.manage_users, name="manage_users"),
     path('create_group/', views.create_group, name='create_group'),
     path('account/', views.account, name='account'),
