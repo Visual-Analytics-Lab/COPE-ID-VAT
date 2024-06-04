c80bf322df4d7adeed5613f6a13a533d038d8c2a
jdowdy11 <46544893+jdowdy11@users.noreply.github.com>
2024-06-04 13:54:53 -0500

    added functionality to pull topic names from the db. initial models created for adding a project and coding variables and their values

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Tutorial, sample_data
+from .models import Tutorial, sample_data, bert_main_sample_data
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import logout, authenticate, login
 from django.contrib import messages
@@ -9,12 +9,16 @@ from django.core.paginator import Paginator
 # Create your views here.
 def homepage(request):
     distinct_platforms = sample_data.objects.values_list('doc_source', flat=True).distinct()
+    distinct_topics = bert_main_sample_data.objects.values_list('topic_name', flat=True).distinct()
     return render(request = request,
                   template_name='main/home.html',
                   context = {"tutorials":Tutorial.objects.all,
-                             "distinct_platforms": distinct_platforms},)
+                             "distinct_platforms": distinct_platforms,
+                             "distinct_topics": distinct_topics})
 
 def addProject(request):
+    
+
     return render(request = request,
                   template_name='main/addProject.html',
                   context = {"tutorials":Tutorial.objects.all})
@@ -48,58 +52,6 @@ def users(request):
     return render(request = request,
                   template_name='main/users.html',
                   context = {"tutorials":Tutorial.objects.all})
-
-
-
-
-# def register(request):
-#     if request.method == "POST":
-#         form = NewUserForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             username = form.cleaned_data.get('username')
-#             messages.success(request, f"New account created: {username}")
-#             login(request, user)
-#             return redirect("main:homepage")
-
-#         else:
-#             for msg in form.error_messages:
-#                 messages.error(request, f"{msg}: {form.error_messages[msg]}")
-
-#             return render(request = request,
-#                           template_name = "main\\register.html",
-#                           context={"form":form})
-
-#     form = NewUserForm
-#     return render(request = request,
-#                   template_name = "main\\register.html",
-#                   context={"form":form})
-
-# def logout_request(request):
-#     logout(request)
-#     messages.info(request, "Logged out successfully")
-#     return redirect("main:homepage")
-
-
-# def login_request(request):
-#     if request.method == 'POST':
-#         form = AuthenticationForm(request=request, data=request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data.get('password')
-#             user = authenticate(username=username, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 messages.info(request, f"You are now logged in as {username}")
-#                 return redirect('/')
-#             else:
-#                 messages.error(request, "Invalid username or password.")
-#         else:
-#             messages.error(request, "Invalid username or password.")
-#     form = AuthenticationForm()
-#     return render(request = request,
-#                     template_name = "main/login.html",
-#                     context={"form":form})
 
 def datagrid(request):
     search_query = request.GET.get('search_query')
