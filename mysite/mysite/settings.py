83bcabad09449013f4c7e0d92d9ead8e2a3aeb24
Jacob Adams <jra457@msstate.edu>
2024-07-19 12:30:27 -0500

    Set my_profile_model as auth profile model in settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -100,6 +100,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'user_management.my_profile_model'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
