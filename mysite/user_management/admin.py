013576e606ffe98158a33bd05cadee191a3b6ff1
Jacob Adams <jra457@msstate.edu>
2024-07-25 13:09:37 -0500

    Fixed password form in admin dashboard

--- mysite/user_management/admin.py
+++ mysite/user_management/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import my_profile_model
 
-admin.site.register(my_profile_model)
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = my_profile_model
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = my_profile_model
+
+class MyUserAdmin(UserAdmin):
+    form = MyUserChangeForm
+    add_form = MyUserCreationForm
+    model = my_profile_model
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('title', 'department', 'organization', 'city', 'state', 'zip_code', 'country')}),
+    )
+
+admin.site.register(my_profile_model, MyUserAdmin)
