8dd006ef9194ddacc142971a6bea3108d7ee5fc0
Jacob <jra457@msstate.edu>
2025-07-23 17:49:28 -0400

    Fixed directory for config file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -18,7 +18,7 @@ import yaml
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Load config
-with open(os.path.join(BASE_DIR, 'mysite/config.yml'), 'r') as f:
+with open(os.path.join(BASE_DIR, 'mysite\\config.yml'), 'r') as f:
     config = yaml.safe_load(f)
 
 # Quick-start development settings - unsuitable for production
